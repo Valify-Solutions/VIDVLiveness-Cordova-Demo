c494f1c9d61da087db47c75ee4bb017563dbf861
Mohamed Baher <mohamed.baher@valify.me>
2024-09-29 15:17:03 +0300

    initial

---
+++ platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -0,0 +1,15 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+  module.exports = [
+    {
+      "id": "cordova-plugin-vidvliveness.VIDVLivenessPlugin",
+      "file": "plugins/cordova-plugin-vidvliveness/www/VIDVLivenessPlugin.js",
+      "pluginId": "cordova-plugin-vidvliveness",
+      "clobbers": [
+        "VIDVLivenessPlugin"
+      ]
+    }
+  ];
+  module.exports.metadata = {
+    "cordova-plugin-vidvliveness": "1.0.1"
+  };
+});
---
+++ platforms/android/app/src/main/assets/www/js/index.js
@@ -0,0 +1,70 @@
+const token = "rtMuIhjslZwZUqkT3qOopO80BML4BO";
+const baseUrl = "https://www.valifystage.com/";
+const bundleKey = "ee3a3dfa43734ac1b416c5729d3f3aef";
+const language = "en";
+const primaryColor = "#000000"; // Assuming a primary color
+
+const testLiveness = true;
+
+
+// Liveness specific parameters
+const livenessParams = {
+access_token: token,
+base_url: baseUrl,
+bundle_key: bundleKey,
+language: language,
+primary_color: primaryColor,
+enable_smile: true,
+enable_look_left: true,
+enable_look_right: true,
+enable_close_eyes: true,
+livenss_number_of_failed_trials: "3",
+livness_number_of_instructions: "4",
+liveness_time_per_action: "10",
+enable_voiceover: true,
+show_error_message: true,
+enable_logging: true
+};
+// Start process function refactored to only initialize relevant SDK based on condition
+function startProcess() {
+if (testLiveness){
+        console.log("Starting Liveness Check");
+        window.VIDVLivenessPlugin.startLiveness(livenessParams, null, null, function(result) {
+            const s = result.toString();
+            const jsonResult = JSON.parse(s);
+            console.log("Liveness Success:", jsonResult);
+            const state = jsonResult.nameValuePairs.state;
+            switch (state) {
+                case "SUCCESS":
+                    console.log("Liveness was successful.");
+                    // Add more logic here as necessary
+                    break;
+                case "CAPTURED_IMAGES":
+                    console.log("Live captured images (one per time)");
+                    // Add more logic here as necessary
+                    break;
+            }
+        }, function(error) {
+            const s = error.toString();
+            const jsonResult = JSON.parse(s);
+            console.error("Liveness Error:", jsonResult);
+            const state = jsonResult.nameValuePairs.state;
+            switch (state) {
+                case "ERROR":
+                    console.log("A Builder Error");
+                    // Add more logic here as necessary
+                    break;
+                case "FAILURE":
+                    console.log("A Service Failure");
+                    // Add more logic here as necessary
+                    break;
+                case "EXIT":
+                    console.log("Process was exited by the user.");
+                    // Add more logic here as necessary
+                    break;
+            }
+        });
+    }
+}
+
+document.getElementById("button").addEventListener("click", startProcess);
